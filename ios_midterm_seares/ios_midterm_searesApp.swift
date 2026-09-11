674a616ee4a668d8d544b2e1f486c634414de164
Mike Andrei Gomez <allenseares2@gmail.com>
2026-09-11 11:02:18 +0800

    Initial commit

---
+++ ios_midterm_seares/ios_midterm_searesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_midterm_searesApp.swift
+//  ios_midterm_seares
+//
+//  Created by STUDENT on 9/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_midterm_searesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
